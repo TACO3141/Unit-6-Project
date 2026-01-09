0b94e81dc53c95e5f21149d1fe526120d9b23918
TACO3141 <andrewej10@gmail.com>
2026-01-09 11:42:52 -0700

    Create StudentStatsArray.java

---
+++ StudentStatsArray.java
@@ -0,0 +1,16 @@
+public class StudentStatsArray
+{
+  private final Student[] students;
+  public StudentStatsArray(Student[] students)
+  {
+    this.students = students;
+  }
+
+  public double averageGrade()
+  {
+    for (i = 0; i < students.length - 1; i++)
+    {
+      
+    }
+  }
+}
