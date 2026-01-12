d387f20ebe0ae5e7ba819453f061232e8e2b8eec
TACO3141 <andrewej10@gmail.com>
2026-01-12 10:56:43 -0700

    Update StudentStatsArray.java

--- StudentStatsArray.java
+++ StudentStatsArray.java
@@ -8,9 +8,13 @@ public class StudentStatsArray
 
   public double averageGrade()
   {
+    double avg = 0.0;
     for (i = 0; i < students.length - 1; i++)
     {
-      
+      avg += students[i].getCurrentGrade();
     }
+    avg /= 2;
+    return avg;
   }
+  
 }
